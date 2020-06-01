7403bdd8fda4c1c3235224fec39feb6187bb40a7
Mihail Shcherbakov <mihail.shch@gmail.com>
2020-06-01 15:02:46 +0300

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props: any){
+    super(props);
+  }
+  
+  render(){
+    return(
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,8 +24,9 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+  </div>
   );
+  }
 }
 
 export default App;
